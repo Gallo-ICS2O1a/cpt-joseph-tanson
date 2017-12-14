fb884e03f23365e1686a2035441cc0f02bf4e7c0
josephperez20 <33940135+josephperez20@users.noreply.github.com>
2017-12-14 14:01:43 -0500

    Add ball physics
    Add basic bullet physics

--- my-app.py
+++ my-app.py
@@ -1,7 +1,24 @@
-# Template for Processing sketches.
-
+screen = 100
+ball = PVector(100, 100)
+speeds = []
+bullets = []
 def setup():
-  size(400, 400)
-  
- def draw():
-  background(255)
+    size(int(screen * 7.5), int(screen * 6))
+def trajectory(v1, v2):
+    difference = PVector.sub(v1, v2)
+    angle = difference.heading()
+    return PVector.fromAngle(angle)
+def draw():
+    global speed
+    background(255)
+    mouse = PVector(mouseX, mouseY)
+    ellipse(ball.x, ball.y, 50, 50)
+    point(mouse.x, mouse.y)
+    for i in range(len(bullets)):
+        ellipse(bullets[i].x, bullets[i].y, 5, 5)
+        bullets[i].add(speeds[i])
+def mousePressed():
+    global speed, mouse, ball
+    mouse = PVector(mouseX, mouseY)
+    bullets.append(PVector(ball.x, ball.y))
+    speeds.append(trajectory(mouse, ball).mult(7))
