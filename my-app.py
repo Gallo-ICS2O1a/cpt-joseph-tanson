708fe9a382867fbe2dac32f66b705f86b021d34c
josephperez20 <33940135+josephperez20@users.noreply.github.com>
2017-12-14 14:20:34 -0500

    Add slime collision

--- my-app.py
+++ my-app.py
@@ -1,7 +1,8 @@
 screen = 100
-ball = PVector(100, 100)
+player = PVector(100, 100)
 speeds = []
 bullets = []
+slimes = [PVector(random(25, 750), random(25, 550))]
 def setup():
     size(int(screen * 7.5), int(screen * 6))
 def trajectory(v1, v2):
@@ -12,13 +13,16 @@ def draw():
     global speed
     background(255)
     mouse = PVector(mouseX, mouseY)
-    ellipse(ball.x, ball.y, 50, 50)
+    ellipse(player.x, player.y, 50, 50)
     point(mouse.x, mouse.y)
     for i in range(len(bullets)):
         ellipse(bullets[i].x, bullets[i].y, 5, 5)
         bullets[i].add(speeds[i])
-def mousePressed():
-    global speed, mouse, ball
+        if dist(bullets[i].x, bullets[i].y, slimes[0].x, slimes[0].y) < 25:
+            slimes[0].x, slimes[0].y = random(25, 750), random(25, 500)
+    ellipse(slimes[0].x, slimes[0].y, 25, 25)
+def keyPressed(q):
+    global speed, mouse, player
     mouse = PVector(mouseX, mouseY)
-    bullets.append(PVector(ball.x, ball.y))
-    speeds.append(trajectory(mouse, ball).mult(7))
+    bullets.append(PVector(player.x, player.y))
+    speeds.append(trajectory(mouse, player).mult(7))
