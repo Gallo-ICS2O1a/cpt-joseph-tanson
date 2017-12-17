541a6bf72a169664cefc00c65d62e458fb16c620
tanson-lee <33968771+tanson-lee@users.noreply.github.com>
2017-12-16 22:16:38 -0500

    created play button and attempts at other parts
    attempt at slime movement and slow-mo

--- slime game progress.py
+++ slime game progress.py
@@ -1,115 +1,3 @@
-screen = 100
-player = PVector(100, 100)
-speeds = []
-bullets = []
-slimes = [PVector(random(25, 750), random(25, 550))]
-
-
-def setup():
-    size(int(screen * 7.5), int(screen * 6))
-    
-    
-def trajectory(v1, v2):
-    difference = PVector.sub(v1, v2)
-    angle = difference.heading()
-    return PVector.fromAngle(angle)
-
-
-def draw():
-    global speed
-    background(255)
-    
-    mouse = PVector(mouseX, mouseY)
-    ellipse(player.x, player.y, 50, 50)
-    point(mouse.x, mouse.y)
-    
-    for i in range(len(bullets)):
-        ellipse(bullets[i].x, bullets[i].y, 5, 5)
-        bullets[i].add(speeds[i])
-        if dist(bullets[i].x, bullets[i].y, slimes[0].x, slimes[0].y) < 25:
-            slimes[0].x, slimes[0].y = random(25, 750), random(25, 500)
-    ellipse(slimes[0].x, slimes[0].y, 25, 25)
- 
- 
-def keyPressed(q):
-    global speed, mouse, player
-    mouse = PVector(mouseX, mouseY)
-    bullets.append(PVector(player.x, player.y))
-    speeds.append(trajectory(mouse, player).mult(7))
-    
-    
-# diff code
-# diff code
-rectangle = PVector(100, 100)
-clicked = False
-
-def setup():
-    size(400, 400)
-    
-def mouseClicked():
-    global clicked
-    
-    if clicked:
-        clicked = False
-    if not clicked:    
-        clicked = True
-    
-def draw():
-
-    if clicked:
-        if mouseX > rectangle.x and mouseX < rectangle.x + 100 and mouseY > rectangle.y and mouseY < rectangle.y + 100:
-            background(0)
-    
-    elif not clicked:
-        background(255)
-        
-
-    fill(0)
-    rect(rectangle.x, rectangle.y, 100, 100)
-    fill(255)
-    textSize(16)
-    text('play', rectangle.x + 40, rectangle.y + 50)
-    
-    # diff code
-    # diff code
-player = PVector(200, 200)
-player_size = 100
-player_speed = PVector(0, 0)
-
-
-
-def setup():
-    size(400, 400)
-    
-def draw():
-    background(255)
-    fill(0)
-    noStroke()
-    
-    player.add(player_speed)
-    ellipse(player.x, player.y, player_size, player_size)
-    
-
-def keyPressed():
-    
-    global w, a, s, d
-    
-    if key == 'w':
-        player_speed.y -= 5
-    if key == 'a':
-        player_speed.x -= 5
-    if key == 's':
-        player_speed.y += 5
-    if key == 'd':
-        player_speed.x += 5
-        
-def keyReleased():
-    if key == 'w' or key == 's':
-        player_speed.y = 0
-    if key == 'a' or key == 'd':
-        player_speed.x = 0
-        
-        
 # moving and shooting added together
 # moving and shooting added together
 # moving and shooting added together
@@ -176,14 +64,18 @@ def keyReleased():
         player_speed.y = 0
     if key == 'a' or key == 'd':
         player_speed.x = 0
+
         
-        
-#
-#
+# button and attempt in slime movement, slow_mo
+# button and attempt in slime movement, slow_mo
+# button and attempt in slime movement, slow_mo
+
 screen = 100
 bullets_speed = []
 bullets = []
 slimes = [PVector(random(25, 750), random(25, 550))]
+slimes_speed = PVector(1, 1)
+score = 0
 
 player = PVector(200, 200)
 player_size = 100
@@ -192,9 +84,19 @@ player_speed = PVector(0, 0)
 barrier = PVector(100, 100)
 barrier_size = PVector(100, 500)
 
+slow_mo = False
+
+width = int(screen * 7.5)
+height = int(screen * 6)
+
+button_location = PVector((width / 2) - 50, (height / 2))
+button_clicked = False
+button_size = PVector(100, 50)
+
 def setup():
     size(int(screen * 7.5), int(screen * 6))
-    
+
+
 def trajectory(v1, v2):
     difference = PVector.sub(v1, v2)
     angle = difference.heading()
@@ -203,56 +105,97 @@ def trajectory(v1, v2):
 def draw():
     background(255)
     fill(0)
+    global speed, score, player_speed, bullets_speed, slimes_speed
+    
+    if slow_mo:
+        bullets_speed *= 2
+        player_speed *= 2
+        slimes_speed *= 2
+        
+    # slimes bouncing off walls
+    if slimes[0].x > width or slimes[0].y < 0:
+        slimes_speed.x *= -1
+    elif slimes[0].y > width or slimes[0].y < 0:
+        slimes_speed.y *= -1
+    
+    
+    if not button_clicked:    
+        fill(0)
+        rect(button_location.x, button_location.y, button_size.x, button_size.y)
+        
+        fill(255)
+        textSize(32)
+        text('PLAY', button_location.x + 10, button_location.y + 30)
 
-    
-    rect(barrier.x, barrier.y, barrier_size.x, barrier_size.y)
-    rect(100, 100, 100, 100)
-    
-    player.add(player_speed)
-    ellipse(player.x, player.y, player_size, player_size)
-    global speed
-    background(255)
-    
-    mouse = PVector(mouseX, mouseY)
-    ellipse(player.x, player.y, 50, 50)
-    point(mouse.x, mouse.y)
-    
-    
-    for i in range(len(bullets)):
-     #   if bullets > barrier.y and :
-        ellipse(bullets[i].x, bullets[i].y, 5, 5)
-        bullets[i].add(bullets_speed[i])
-        if dist(bullets[i].x, bullets[i].y, slimes[0].x, slimes[0].y) < 25:
-            slimes[0].x, slimes[0].y = random(25, 750), random(25, 500)
-    ellipse(slimes[0].x, slimes[0].y, 25, 25)
+    fill(0)
+    if button_clicked:
+        player.add(player_speed)
+        ellipse(player.x, player.y, player_size, player_size)
+        
+        mouse = PVector(mouseX, mouseY)
+        ellipse(player.x, player.y, 50, 50)
+        point(mouse.x, mouse.y)
+        
+        textSize(24)
+        text('Score: ' + str(score), width - 100, 50)
+        
 
+        for i in range(len(bullets)):
+        #   if bullets > barrier.y and :
+            ellipse(bullets[i].x, bullets[i].y, 5, 5)
+            bullets[i].add(bullets_speed[i])
+            if dist(bullets[i].x, bullets[i].y, slimes[0].x, slimes[0].y) < 25:
+                slimes[0].x, slimes[0].y = random(25, 750), random(25, 500)
+                score += 1
+        slimes[0].add(slimes_speed)
+        ellipse(slimes[0].x, slimes[0].y, 25, 25)
+   
 def keyPressed():
     
     global w, a, s, d
     
     if key == 'w':
         player_speed.y -= 5
+        slow_mo = True
+        
     if key == 'a':
         player_speed.x -= 5
+        slow_mo = True
+        
     if key == 's':
         player_speed.y += 5
+        slow_mo = True
+        
     if key == 'd':
         player_speed.x += 5
-      
+        slow_mo = True
+        
+    if key == ' ':
+        global speed, mouse, player
+        mouse = PVector(mouseX, mouseY)
+        bullets.append(PVector(player.x, player.y))
+        bullets_speed.append(trajectory(mouse, player).mult(7))
+
 def mousePressed():
-    global speed, mouse, player
+    global speed, mouse, player, button_clicked
+    
     mouse = PVector(mouseX, mouseY)
     bullets.append(PVector(player.x, player.y))
     bullets_speed.append(trajectory(mouse, player).mult(7))
+     
+    button_end = PVector(button_location.x + button_size.x, button_location.y + button_size.y)
+    if mouseX in range(int(button_location.x), int(button_end.x)) and mouseY in range(int(button_location.y), int(button_end.y)):
+        button_clicked = True
+    
         
 def keyReleased():
     if key == 'w' or key == 's':
         player_speed.y = 0
+        slow_mo = False
+        
     if key == 'a' or key == 'd':
         player_speed.x = 0
-
-
-    
+        slow_mo = False
     
 
 
