a38aa4697f3603eddc694e9dd52ec98b0084a8f5
josephperez20 <33940135+josephperez20@users.noreply.github.com>
2018-01-04 18:20:01 -0500

    Add slime and player classes to shucks file

--- shucks.py
+++ shucks.py
@@ -5,6 +5,17 @@ def trajectory(v1, v2):
 
 class Bullet:
     def __init__(self, v1, v2):
-        self.location = PVector(v1.x, v1.y)
-        self.speed = trajectory(v1, v2).mult(7)
+        self.lo = PVector(v1.x, v1.y)
+        self.sp = trajectory(v1, v2).mult(7)
         self.done = False
+        
+class Player:
+    def __init__(self, lo, si):
+        self.lo = PVector(lo.x, lo.y)
+        self.si = si
+
+class Slime:
+    def __init__(self, lo, si, sp):
+        self.lo = PVector(lo.x, lo.y)
+        self.si = si
+        self.sp = PVector(sp.x, sp.y)
