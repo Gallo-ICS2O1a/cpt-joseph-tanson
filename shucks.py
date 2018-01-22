786c272d28aef22b4cdea0685545446545a3ffba
josephperez20 <33940135+josephperez20@users.noreply.github.com>
2018-01-22 12:34:05 -0500

    PEP-8 compliant

--- shucks.py
+++ shucks.py
@@ -3,17 +3,20 @@ def trajectory(v1, v2):
     angle = difference.heading()
     return PVector.fromAngle(angle).mult(-1)
 
+
 class Bullet:
     def __init__(self, v1, v2, isPlayer):
         self.lo = PVector(v1.x, v1.y)
         self.sp = trajectory(v1, v2).mult(7)
         self.done = False
         self.p = isPlayer
-        
+
+
 class Player:
     def __init__(self, lo, si):
         self.lo = PVector(lo.x, lo.y)
         self.si = si
+
 
 class Slime:
     def __init__(self, lo, v2):
